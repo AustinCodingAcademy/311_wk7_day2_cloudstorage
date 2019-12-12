3c561b59b2994860f59697f49f9b3879e3a59ee0
JustinBarsalou <justin.barsalou@g.austincc.edu>
2019-12-11 19:50:32 -0600

    finished extra credit, got a  good boy in my bucket

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const fs = require("fs");
 const {Storage} = require('@google-cloud/storage');
 
 const storage = new Storage({
-  projectId: 'aca-311',
-  keyFilename: './aca-311-d864d06c299f.json'
+  projectId: 'concrete-kayak-258101',
+  keyFilename: './first-instance-0911220e2eac.json'
 })
 
 
-storage.bucket("aca-311-files").upload("./bruce.jpg", {
+storage.bucket("justinbarsalou_aca-harddrive").upload("./bruce.jpg", {
     gzip: true,
     metadata: {
         cacheControl: 'public, max-age=31536000',
